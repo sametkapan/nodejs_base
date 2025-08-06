ea63833126754da5c5faadf52eeb5e54c32eb54c
Samet Kapan <kapan13201@gmail.com>
2025-08-06 11:47:56 +0300

    initial commit2

--- nodejs_base_api/app.js
+++ nodejs_base_api/app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use((req, res, next) => {
-  console.log("NABER NABER NABER");
+  console.log("NABERRR NABER NABER");
   next();
 });
 
